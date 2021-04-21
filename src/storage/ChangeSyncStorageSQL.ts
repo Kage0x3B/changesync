693c14032620cb24ee65c26340b1b2862be8c5fc
Kage0x3B <moritz.hein@live.de>
2021-04-21 21:29:41 +0200

    Fix other sql query

--- src/storage/ChangeSyncStorageSQL.ts
+++ src/storage/ChangeSyncStorageSQL.ts
@@ -62,7 +62,7 @@ export class ChangeSyncStorageSQL<DataType> extends ChangeSyncStorage<DataType> 
             await this.sqlStorage.executeQuery('UPDATE changelog SET received = TRUE WHERE id IN ?', [[idList]]);
         } else {
             await this.sqlStorage.executeQuery('UPDATE changelog SET resend_count = resend_count + 1 WHERE id IN ?', [
-                idList
+                [idList]
             ]);
         }
     }
